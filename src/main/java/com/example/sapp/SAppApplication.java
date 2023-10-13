fd09553de50e73a610ddc020a59bd2bd3de53d95
mateS124 <mah83@wp.pl>
2023-10-13 22:39:26 +0200

    SpringSecurityApp

---
+++ src/main/java/com/example/sapp/SAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/sapp/security/AppUserRole.java
@@ -0,0 +1,35 @@
+package com.example.sapp.security;
+
+import com.google.common.collect.Sets;
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.example.sapp.security.ApplicationUserPermission.*;
+
+@Getter
+public enum AppUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_WRITE, COURSE_READ, STUDENT_READ, STUDENT_WRITE)),
+    ADMINTRAINEE(Sets.newHashSet(COURSE_READ, STUDENT_READ));
+
+    private final Set<ApplicationUserPermission> permissions;
+
+    AppUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions =  getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE" + this.name()));
+        return permissions;
+    }
+}
